ae445a50b722f5bd87087c981857b197217faa47
Дмитрий Баранов <demon.asgard@gmail.com>
2012-08-29 01:37:35 +0600

    Добавлен генератор матриц с диагональным преобладанием (на python).

---
+++ solecl/matrix/gen.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import random
+
+random.seed()
+
+nlist = [1000]
+
+for n in nlist:
+	M = n*10
+	f = open('matrix%d.txt' % n, 'w')
+	for i in xrange(n):
+		s = ''
+		d = random.random()*M # Диагональный элемент (для соблюдения условия диагонального преобладания)
+		r = d
+		for j in xrange(n):
+			if(i == j):
+				s += '%f ' % d
+			else:
+				t = random.random()*r/(n-j)
+				r -= t
+				s += '%f ' % t
+		s += '%f\n' % (random.random()*M)
+		f.write(s)
+	f.close()
+	print(n)
