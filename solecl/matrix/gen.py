c7e526a17d4f5ccff79172cc4ba73db440a7851b
Дмитрий Баранов <demon.asgard@gmail.com>
2012-09-02 18:37:44 +0600

    Генератор теперь генерирует матрицы размерности, заданной в качестве аргументов командной строки. Точность составяет 2 знака после запятой.

--- solecl/matrix/gen.py
+++ solecl/matrix/gen.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import random
+import random, sys
 
 random.seed()
 
-nlist = [10, 100, 1000, 10000]
+nlist = map(lambda i: int(i), sys.argv[1:])
 
 for n in nlist:
 	M = 10
@@ -12,8 +12,8 @@ for n in nlist:
 	for i in xrange(n):
 		s = ''
 		for j in xrange(n):
-			s += '%f ' % (random.random() * M * (-1 if random.randint(1, 2)%2 == 0 else 1))
-		s += '%f\n' % (random.random() * M * 10 * (-1 if random.randint(1, 2)%2 == 0 else 1))
+			s += '%.2f ' % (random.random() * M * (-1 if random.randint(1, 2)%2 == 0 else 1))
+		s += '%.2f\n' % (random.random() * M * 10 * (-1 if random.randint(1, 2)%2 == 0 else 1))
 		f.write(s)
 	f.close()
 	print(n)
