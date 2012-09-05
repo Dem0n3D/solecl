e62f6c1e5b868758eeb329f2740a62543b7637be
Дмитрий Баранов <demon.asgard@gmail.com>
2012-09-06 00:30:19 +0600

    Тестирование больших матриц.

--- solecl/main.cpp
+++ solecl/main.cpp
@@ -20,7 +20,6 @@ int main(int argc, char *argv[])
     int N;
     cout << "Enter N: ";
     cin >> N;
-//    N = 100;
 
     stringstream fname;
     fname << "matrix/matrix" << N << ".txt";
@@ -51,10 +50,10 @@ int main(int argc, char *argv[])
     QVector<float> x1(N, 0);
     QVector<float> x2(N, 0);
 
-    QVector< QVector<float> > AtA(N, QVector<float>(N+1, 0));
-    QCLBuffer buffAtA;
-    t0 = multTranspCL(A, AtA, N, context, &buffAtA);
-    t1 = Zeidel(AtA, N, x1, 0.1);
+    //QVector< QVector<float> > AtA(N, QVector<float>(N+1, 0));
+    //QCLBuffer buffAtA;
+    //t0 = multTranspCL(A, AtA, N, context, &buffAtA);
+    //t1 = Zeidel(AtA, N, x1, 0.1);
 
     QCLBuffer buffA = context->createBufferDevice(N*(N+1)*sizeof(float), QCLMemoryObject::ReadWrite);
 
@@ -68,30 +67,35 @@ int main(int argc, char *argv[])
     res << "+==================================================+\n\n";
     res << setprecision(4);
 
-    int test_count = (N > 1000) ? 3 : 5;
+    int test_count = (N > 1000) ? 1 : 5;
 
     int avg, i1;
 
-    for(int i = 0; i < (N+1); i += 32) {
+    int maxSize = context->defaultDevice().maximumWorkItemsPerGroup();
+
+    for(int i = (N > 4000) ? 32 : 0; i <= (N+1)/3 && i <= maxSize; i += 32) {
         avg = 0;
         i1 = (i == 0) ? 1 : i;
         res << N << ' ' << i1 << ' ' << (N+1-((N+1)/i1)*i1) << ' ';
 
+        if(N > 1000 && i > 64)
+            test_count = 2;
+
         for(int j = 0; j < test_count; j++) {
             matrix2CLBuff(A, buffA);
             t2 = GaussCL(buffA, N, xcl, i1, context);
-            xcl.read(&x2[0], N);
+            /*xcl.read(&x2[0], N);
 
             float max = 0;
             for(int k = 0; k < N; k++) {
                 max = (max > fabs(x1[k]-x2[k])) ? max : fabs(x1[k]-x2[k]);
-            }
+            }*/
 
             avg += t2;
 
-            qDebug() << i1 << j << t2 << max;
+            qDebug() << i1 << j << t2 << -1;
 
-            res << t2 << ' ' << max << ' ';
+            res << t2 << ' ' << -1 << ' ';
         }
 
         res << avg/float(test_count) << ' ' << endl;
